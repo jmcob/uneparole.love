31ae0cc975c3bb494f2c7521d855470c9da6a8e1
Jean-Maxime <ola@jmax.dev>
2022-08-15 23:30:37 +0200

    vuetify added, vtabs added

--- src/components/Informations.vue
+++ src/components/Informations.vue
@@ -1,10 +1,12 @@
 <template>
-  <p>Actuellement {{ paroles.length }} paroles différentes.</p>
+  <p>Il y a actuellement {{ paroles.length }} paroles différentes.</p>
+  <br />
   <p>
     Les extraits ont été initialement choisis pour l'adoration eucharistique :
     sous la houlette et avec la participation active de la coordinatrice de
     l’adoration perpétuelle au Sacré-Cœur à Bordeaux.
   </p>
+  <br />
   <p>
     Version 1.1, août 2022 – A terme, 65 extraits courants de la Bible puis 83
     extraits de psaumes,
@@ -16,10 +18,20 @@
       >Télécharger le PDF complet des paroles de Dieu sélectionnées</a
     >
   </p>
+  <br />
   <p>
     Ici vous êtes libres : pas de télémétrie, pas de cookie, pas de pub, pas de
     suivi inter-site, pas de copyright, un simple service, que du bonheur !
   </p>
+  <br />
+  <p>
+    Voici le lien pour
+    <router-link class="about" to="/contact"
+      >soumettre une nouvelle parole</router-link
+    >
+    de la Bible sur le site.
+  </p>
+  <br />
   <p><a href="http://jmxi.art">jeanmaxime</a></p>
 </template>
 
