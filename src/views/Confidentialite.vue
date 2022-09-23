4481e21de9d81c7e6fda3aaee6c49ae2795b12b8
Jean-Maxime <ola@jmax.dev>
2022-09-24 00:51:50 +0200

    maj confid

--- src/views/Confidentialite.vue
+++ src/views/Confidentialite.vue
@@ -8,20 +8,17 @@
         <p>
           <br />
           <h3>1. Les hébergeurs nous connaissent</h3>
-          Ce site en sa base de données est hébergé par Google ; le serveur de
-          cette page est sur un hébergeur gratuit : Vercel. Ces hébergeurs
+          Ce site en sa base de données est hébergé par Vercel ; le serveur de
+          cette page est sur le même hébergeur gratuit : Vercel. Cet hébergeur
           surveillent nos pas par l'adresse IP.
           <br />
           <br>
           <h3>2. Pas de suivi ici</h3>
           Je n'ai installé aucune télémétrie sur le site pour la consultation de
-          la Parole de Dieu, ni aucun cookie, Google Analytics est désactivé. Je
-          sais juste, de par la facturation de Google, le nombre de lectures
-          faites sur la base de donnée. Aussi les utilisateurs modérateurs
-          passent par la connexion de Google.
+          la Parole de Dieu, ni aucun cookie, Google Analytics n'est pas installé. 
           <br /><br />
-          Je sais aussi, parce que le site est sur la recherche de Google, quels
-          termes mènent ici et en quelle quantité.
+          Je sais juste, parce que le site est sur la recherche de Google, quels
+          termes mènent ici et en quelle quantité, et encore pour le moment ce sont des chiffres très petits.
                     <br />
           <br>
           <h3>3. Un humble service</h3>
